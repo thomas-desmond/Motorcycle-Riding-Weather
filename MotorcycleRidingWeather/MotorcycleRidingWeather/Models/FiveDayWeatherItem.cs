1dc12603062bd4fa40af25c6ab762b20032a7567
Thomas Desmond <thomasdes533@gmail.com>
2018-05-25 19:31:17 -0700

    initial commit with very basic ListView functionality

---
+++ MotorcycleRidingWeather/MotorcycleRidingWeather/Models/FiveDayWeatherItem.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MotorcycleRidingWeather.Models
+{
+    public partial class FiveDayWeatherItem
+    {
+        [JsonProperty("cod")]
+        public string Cod { get; set; }
+
+        [JsonProperty("message")]
+        public double Message { get; set; }
+
+        [JsonProperty("cnt")]
+        public long Cnt { get; set; }
+
+        [JsonProperty("list")]
+        public List<DataList> DataList { get; set; }
+
+        [JsonProperty("city")]
+        public City City { get; set; }
+    }
+
+    public partial class City
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("coord")]
+        public Coord Coord { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+    }
+
+    public partial class Coord
+    {
+        [JsonProperty("lat")]
+        public double Lat { get; set; }
+
+        [JsonProperty("lon")]
+        public double Lon { get; set; }
+    }
+
+    public partial class DataList
+    {
+        [JsonProperty("dt")]
+        public long Dt { get; set; }
+
+        [JsonProperty("main")]
+        public MainClass Main { get; set; }
+
+        [JsonProperty("weather")]
+        public List<Weather> Weather { get; set; }
+
+        [JsonProperty("clouds")]
+        public Clouds Clouds { get; set; }
+
+        [JsonProperty("wind")]
+        public Wind Wind { get; set; }
+
+        [JsonProperty("rain")]
+        public Rain Rain { get; set; }
+
+        [JsonProperty("sys")]
+        public Sys Sys { get; set; }
+
+        [JsonProperty("dt_txt")]
+        public string DateTimeAsString { get; set; }
+    }
+
+    public partial class Clouds
+    {
+        [JsonProperty("all")]
+        public long All { get; set; }
+    }
+
+    public partial class MainClass
+    {
+        [JsonProperty("temp")]
+        public double Temp { get; set; }
+
+        [JsonProperty("temp_min")]
+        public double TempMin { get; set; }
+
+        [JsonProperty("temp_max")]
+        public double TempMax { get; set; }
+
+        [JsonProperty("pressure")]
+        public double Pressure { get; set; }
+
+        [JsonProperty("sea_level")]
+        public double SeaLevel { get; set; }
+
+        [JsonProperty("grnd_level")]
+        public double GrndLevel { get; set; }
+
+        [JsonProperty("humidity")]
+        public long Humidity { get; set; }
+
+        [JsonProperty("temp_kf")]
+        public double TempKf { get; set; }
+    }
+
+    public partial class Rain
+    {
+        [JsonProperty("3h", NullValueHandling = NullValueHandling.Ignore)]
+        public double? The3H { get; set; }
+    }
+
+    public partial class Sys
+    {
+        [JsonProperty("pod")]
+        public Pod Pod { get; set; }
+    }
+
+    public partial class Weather
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("main")]
+        public MainEnum Main { get; set; }
+
+        [JsonProperty("description")]
+        public Description Description { get; set; }
+
+        [JsonProperty("icon")]
+        public Icon Icon { get; set; }
+    }
+
+    public partial class Wind
+    {
+        [JsonProperty("speed")]
+        public double Speed { get; set; }
+
+        [JsonProperty("deg")]
+        public double Deg { get; set; }
+    }
+
+    public enum Pod { D, N };
+
+    public enum Description { BrokenClouds, ClearSky, LightRain };
+
+    public enum Icon { The01D, The01N, The02N, The04N, The10D, The10N };
+
+    public enum MainEnum { Clear, Clouds, Rain };
+
+    public partial class FiveDayWeatherItem
+    {
+        public static FiveDayWeatherItem FromJson(string json) => JsonConvert.DeserializeObject<FiveDayWeatherItem>(json, MotorcycleRidingWeather.Models.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this FiveDayWeatherItem self) => JsonConvert.SerializeObject(self, MotorcycleRidingWeather.Models.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                new PodConverter(),
+                new DescriptionConverter(),
+                new IconConverter(),
+                new MainEnumConverter(),
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class PodConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(Pod) || t == typeof(Pod?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            switch (value)
+            {
+                case "d":
+                    return Pod.D;
+                case "n":
+                    return Pod.N;
+            }
+            throw new Exception("Cannot unmarshal type Pod");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            var value = (Pod)untypedValue;
+            switch (value)
+            {
+                case Pod.D:
+                    serializer.Serialize(writer, "d"); return;
+                case Pod.N:
+                    serializer.Serialize(writer, "n"); return;
+            }
+            throw new Exception("Cannot marshal type Pod");
+        }
+    }
+
+    internal class DescriptionConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(Description) || t == typeof(Description?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            switch (value)
+            {
+                case "broken clouds":
+                    return Description.BrokenClouds;
+                case "clear sky":
+                    return Description.ClearSky;
+                case "light rain":
+                    return Description.LightRain;
+            }
+            throw new Exception("Cannot unmarshal type Description");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            var value = (Description)untypedValue;
+            switch (value)
+            {
+                case Description.BrokenClouds:
+                    serializer.Serialize(writer, "broken clouds"); return;
+                case Description.ClearSky:
+                    serializer.Serialize(writer, "clear sky"); return;
+                case Description.LightRain:
+                    serializer.Serialize(writer, "light rain"); return;
+            }
+            throw new Exception("Cannot marshal type Description");
+        }
+    }
+
+    internal class IconConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(Icon) || t == typeof(Icon?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            switch (value)
+            {
+                case "01d":
+                    return Icon.The01D;
+                case "01n":
+                    return Icon.The01N;
+                case "02n":
+                    return Icon.The02N;
+                case "04n":
+                    return Icon.The04N;
+                case "10d":
+                    return Icon.The10D;
+                case "10n":
+                    return Icon.The10N;
+            }
+            throw new Exception("Cannot unmarshal type Icon");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            var value = (Icon)untypedValue;
+            switch (value)
+            {
+                case Icon.The01D:
+                    serializer.Serialize(writer, "01d"); return;
+                case Icon.The01N:
+                    serializer.Serialize(writer, "01n"); return;
+                case Icon.The02N:
+                    serializer.Serialize(writer, "02n"); return;
+                case Icon.The04N:
+                    serializer.Serialize(writer, "04n"); return;
+                case Icon.The10D:
+                    serializer.Serialize(writer, "10d"); return;
+                case Icon.The10N:
+                    serializer.Serialize(writer, "10n"); return;
+            }
+            throw new Exception("Cannot marshal type Icon");
+        }
+    }
+
+    internal class MainEnumConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(MainEnum) || t == typeof(MainEnum?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            switch (value)
+            {
+                case "Clear":
+                    return MainEnum.Clear;
+                case "Clouds":
+                    return MainEnum.Clouds;
+                case "Rain":
+                    return MainEnum.Rain;
+            }
+            throw new Exception("Cannot unmarshal type MainEnum");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            var value = (MainEnum)untypedValue;
+            switch (value)
+            {
+                case MainEnum.Clear:
+                    serializer.Serialize(writer, "Clear"); return;
+                case MainEnum.Clouds:
+                    serializer.Serialize(writer, "Clouds"); return;
+                case MainEnum.Rain:
+                    serializer.Serialize(writer, "Rain"); return;
+            }
+            throw new Exception("Cannot marshal type MainEnum");
+        }
+    }
+}
---
+++ MotorcycleRidingWeather/MotorcycleRidingWeather/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Navigation;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MotorcycleRidingWeather.Models;
+using System.Collections.ObjectModel;
+
+namespace MotorcycleRidingWeather.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private ObservableCollection<DataList> _weatherCollection;
+        public ObservableCollection<DataList> WeatherCollection
+        {
+            get { return _weatherCollection; }
+            set { SetProperty(ref _weatherCollection, value); }
+        }
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+        }
+
+		public override async void OnNavigatingTo(NavigationParameters parameters)
+		{
+            var fiveDayAllData = await GetWeatherByZipCode();
+            WeatherCollection = new ObservableCollection<DataList>(fiveDayAllData.DataList);
+        }
+
+		internal async Task<FiveDayWeatherItem> GetWeatherByZipCode()
+        {
+            var client = new HttpClient();
+            var httpRequest = new HttpRequestMessage();
+            httpRequest.Method = HttpMethod.Get;
+            httpRequest.RequestUri =
+                new Uri("http://api.openweathermap.org/data/2.5/forecast?zip=94040,us&units=imperial&appid=4bd567dca0d90153a821781e2b6a9574");
+            HttpResponseMessage response = await client.SendAsync(httpRequest);
+            FiveDayWeatherItem fiveDayWeatherItemCollection = null;
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonContent = await response.Content.ReadAsStringAsync();
+                fiveDayWeatherItemCollection = FiveDayWeatherItem.FromJson(jsonContent);
+            }
+            return fiveDayWeatherItemCollection;
+        }
+    }
+}
