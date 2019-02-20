521f8a31812cadbc36a7077fe299c749e03a7bc1
Thomas Desmond <thomasdes533@gmail.com>
2019-02-20 15:43:42 -0800

    Adding new app launch icon

--- MotorcycleRidingWeather/MotorcycleRidingWeather.Android/Resources/Resource.designer.cs
+++ MotorcycleRidingWeather/MotorcycleRidingWeather.Android/Resources/Resource.designer.cs
@@ -6094,10 +6094,7 @@ namespace MotorcycleRidingWeather.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int bike = 2130903040;
-			
-			// aapt resource value: 0x7f030001
-			public const int ic_launcher = 2130903041;
+			public const int ic_launcher = 2130903040;
 			
 			static Mipmap()
 			{
