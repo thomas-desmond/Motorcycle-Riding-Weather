9bc911537f6f55a1e8853904a305e3cc35f7aa39
Thomas Desmond <thomasdes533@gmail.com>
2019-02-21 07:28:39 -0800

    Added splash screen for android. Need to make it less ugly.

--- MotorcycleRidingWeather/MotorcycleRidingWeather.Android/MainActivity.cs
+++ MotorcycleRidingWeather/MotorcycleRidingWeather.Android/MainActivity.cs
@@ -8,13 +8,14 @@ using Prism.Ioc;
 
 namespace MotorcycleRidingWeather.Droid
 {
-    [Activity(Label = "MotorcycleRidingWeather", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MotorcycleRidingWeather", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+            base.SetTheme(Resource.Style.MainTheme);
 
             base.OnCreate(bundle);
 
---
+++ MotorcycleRidingWeather/MotorcycleRidingWeather.Android/SplashActivity.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace MotorcycleRidingWeather.Droid
+{
+    [Activity(Label = "Weather Ride", Icon = "@drawable/ic_launcher", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
