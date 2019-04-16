7671d86a4e5ab3412dc2bf6afa97533763dc9afb
Thomas Desmond <thomasdes533@gmail.com>
2019-04-16 16:26:00 -0700

    New settings button and title on front page also a button to settings page

--- MotorcycleRidingWeather/MotorcycleRidingWeather.Android/Resources/Resource.designer.cs
+++ MotorcycleRidingWeather/MotorcycleRidingWeather.Android/Resources/Resource.designer.cs
@@ -4432,26 +4432,26 @@ namespace MotorcycleRidingWeather.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02014d
-			public const int avd_hide_password_1 = 2130837837;
-			
 			// aapt resource value: 0x7f02014e
-			public const int avd_hide_password_2 = 2130837838;
+			public const int avd_hide_password_1 = 2130837838;
 			
 			// aapt resource value: 0x7f02014f
-			public const int avd_hide_password_3 = 2130837839;
+			public const int avd_hide_password_2 = 2130837839;
+			
+			// aapt resource value: 0x7f020150
+			public const int avd_hide_password_3 = 2130837840;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020150
-			public const int avd_show_password_1 = 2130837840;
-			
 			// aapt resource value: 0x7f020151
-			public const int avd_show_password_2 = 2130837841;
+			public const int avd_show_password_1 = 2130837841;
 			
 			// aapt resource value: 0x7f020152
-			public const int avd_show_password_3 = 2130837842;
+			public const int avd_show_password_2 = 2130837842;
+			
+			// aapt resource value: 0x7f020153
+			public const int avd_show_password_3 = 2130837843;
 			
 			// aapt resource value: 0x7f020055
 			public const int bike = 2130837589;
@@ -5134,11 +5134,11 @@ namespace MotorcycleRidingWeather.Droid
 			// aapt resource value: 0x7f020137
 			public const int notification_icon_background = 2130837815;
 			
-			// aapt resource value: 0x7f02014b
-			public const int notification_template_icon_bg = 2130837835;
-			
 			// aapt resource value: 0x7f02014c
-			public const int notification_template_icon_low_bg = 2130837836;
+			public const int notification_template_icon_bg = 2130837836;
+			
+			// aapt resource value: 0x7f02014d
+			public const int notification_template_icon_low_bg = 2130837837;
 			
 			// aapt resource value: 0x7f020138
 			public const int notification_tile_bg = 2130837816;
@@ -5156,46 +5156,49 @@ namespace MotorcycleRidingWeather.Droid
 			public const int rain32 = 2130837820;
 			
 			// aapt resource value: 0x7f02013d
-			public const int sleet = 2130837821;
+			public const int settings = 2130837821;
 			
 			// aapt resource value: 0x7f02013e
-			public const int snow = 2130837822;
+			public const int sleet = 2130837822;
 			
 			// aapt resource value: 0x7f02013f
-			public const int splashscreen = 2130837823;
+			public const int snow = 2130837823;
 			
 			// aapt resource value: 0x7f020140
-			public const int tooltip_frame_dark = 2130837824;
+			public const int splashscreen = 2130837824;
 			
 			// aapt resource value: 0x7f020141
-			public const int tooltip_frame_light = 2130837825;
+			public const int tooltip_frame_dark = 2130837825;
 			
 			// aapt resource value: 0x7f020142
-			public const int umbrella = 2130837826;
+			public const int tooltip_frame_light = 2130837826;
 			
 			// aapt resource value: 0x7f020143
-			public const int umbrella16 = 2130837827;
+			public const int umbrella = 2130837827;
 			
 			// aapt resource value: 0x7f020144
-			public const int umbrella32 = 2130837828;
+			public const int umbrella16 = 2130837828;
 			
 			// aapt resource value: 0x7f020145
-			public const int wind = 2130837829;
+			public const int umbrella32 = 2130837829;
 			
 			// aapt resource value: 0x7f020146
-			public const int wind16 = 2130837830;
+			public const int wind = 2130837830;
 			
 			// aapt resource value: 0x7f020147
-			public const int wind24 = 2130837831;
+			public const int wind16 = 2130837831;
 			
 			// aapt resource value: 0x7f020148
-			public const int wind32 = 2130837832;
+			public const int wind24 = 2130837832;
 			
 			// aapt resource value: 0x7f020149
-			public const int windIcon = 2130837833;
+			public const int wind32 = 2130837833;
 			
 			// aapt resource value: 0x7f02014a
-			public const int windy = 2130837834;
+			public const int windIcon = 2130837834;
+			
+			// aapt resource value: 0x7f02014b
+			public const int windy = 2130837835;
 			
 			static Drawable()
 			{
